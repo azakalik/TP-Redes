fbcf312b8734aeb8ec662d38bf492c88d6f66c30
Agustin Z <86991224+azakalik@users.noreply.github.com>
2024-05-21 15:47:29 -0300

    Scraping Dockerfile now works

--- webcrawler/src/create_driver.py
+++ webcrawler/src/create_driver.py
@@ -9,6 +9,8 @@ import sys
 def create_driver() -> Chrome:
     try:
         chrome_options = Options()
+        service = webdriver.ChromeService("/opt/chromedriver-linux64/chromedriver")
+        chrome_options.binary_location = '/opt/chrome-linux64/chrome'
 
         # Run Chrome in headless (invisible) mode
         chrome_options.add_argument("--headless")
@@ -23,7 +25,7 @@ def create_driver() -> Chrome:
         chrome_options.add_argument(f"--data-path={mkdtemp()}")
         chrome_options.add_argument(f"--disk-cache-dir={mkdtemp()}")
 
-        driver = webdriver.Chrome(options=chrome_options)
+        driver = webdriver.Chrome(options=chrome_options, service=service)
         driver.maximize_window()
         driver.implicitly_wait(6)  # Global setting
     except Exception as exception:
