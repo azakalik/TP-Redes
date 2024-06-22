2c389f6e0fbdca4e0f8b83ade5cb4064d0766584
sranucci <sranucci@itba.edu.ar>
2024-06-22 17:18:37 -0300

    Changes to webcrawler

--- webcrawler/save_publication_to_cosmos.py
+++ webcrawler/save_publication_to_cosmos.py
@@ -4,26 +4,21 @@ import azure.cosmos.exceptions as exceptions
 from azure.cosmos.partition_key import PartitionKey
 import datetime
 import requests
+import os
 
 from publication_dto import PublicationDTO
 
-def save_publication_to_cosmos(publication_dto):
+def save_publication_to_cosmos(publication_dto: PublicationDTO):
+    # Initialize the Cosmos client using the connection string
+    client = cosmos_client.CosmosClient.from_connection_string(os.environ["CONNECTION_STRING"])
     # Create a dictionary representing the item to be inserted into Cosmos DB
-    azure_function_url = "https://graphqlredeshandlerfinal.azurewebsites.net/api/scrapperHandler"
+    item = publication_dto.to_dict()
 
-
-    headers = {
-        'secret': 'SCRAPPERSECRET',
-        'Content-Type': 'application/json'  # Ensure the content type is set to application/json
-    }
-
+    # Insert the item into Cosmos DB
     try:
-
-        response = requests.post(azure_function_url, json=publication_dto,headers= headers)
-
-        response.raise_for_status()
-        print("Item inserted successfully via Azure Function.")
-    except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
-    except Exception as err:
-        print(f"An error occurred: {err}")
+        client.get_database_client(os.environ["DATABASE_ID"]).get_container_client(os.environ["CONTAINER_ID"]).upsert_item(item)
+        print("Item inserted successfully into Cosmos DB.")
+    except exceptions.CosmosResourceExistsError:
+        print("Item already exists in Cosmos DB.")
+    except exceptions.CosmosHttpResponseError as e:
+        print(f"Failed to insert item into Cosmos DB. Status code: {e.status_code}, Message: {e.message}")
